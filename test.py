cecb180dd881325c1acb2a056e8d2c908ce07cb9
Sai Dhalli <saikiran.dhalli@gmail.com>
2015-06-14 19:51:04 +0000

    -a

--- test.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-
-client = MongoClient();
-db = client.hotspots;
-hotspots_coll = db.twc_hotspots;
-json = { "ueid" : "356567055753949", "model" : "SAMSUNG-SGH-I337", "manufacturer" : "samsung" };
-hotspot_id = hotspots_coll.insert_one(json).inserted_id;
-print hotspot_id;
